52dcfb16aba27265062488a074df8d4a6441a997
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-08-27 16:23:05 +0500

    small tasked added to JS file

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 function addFruit() {
-    let input = document.getElementById("fruitInput");
-    let fruitName = input.value.trim();
+  let input = document.getElementById("fruitInput");
+  let fruitName = input.value.trim();
 
-    if (fruitName === "") {
-        alert("Please enter a fruit name!");
-        return;
-    }
+  if (fruitName === "") {
+    alert("Please enter a fruit name!");
+    return;
+  }
 
-    let ul = document.getElementById("fruitList");
-    let li = document.createElement("li");
+  let ul = document.getElementById("fruitList");
+  let li = document.createElement("li");
 
-    li.innerHTML = `
+  li.innerHTML = `
         <span onclick="toggleComplete(this)">${fruitName}</span>
         <button class="delete" onclick="deleteFruit(this)">X</button>
     `;
 
-    ul.appendChild(li);
-    input.value = "";
+  ul.appendChild(li);
+  input.value = "";
 }
 
 function toggleComplete(element) {
-    element.classList.toggle("completed");
+  element.classList.toggle("completed");
 }
 
 function deleteFruit(button) {
-    button.parentElement.remove();
+  button.parentElement.remove();
 }
